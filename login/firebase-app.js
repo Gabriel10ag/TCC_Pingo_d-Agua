66d14ee911ff9892d665f46321ffdf0d8843e1eb
gabriel10ag <gabrielalves619966@gmail.com>
2024-09-19 16:31:10 -0300

    envio de email

--- login/firebase-app.js
+++ login/firebase-app.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, sendPasswordResetEmail } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
-
 
 const firebaseConfig = {
     apiKey: "AIzaSyDjbdj0Q2GLbTQZK4Qw-I2PZh2Foa0ObPI",
@@ -12,15 +11,12 @@ const firebaseConfig = {
     appId: "1:273265835920:web:4e7e5f50b13c44b4817d15"
 };
 
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-
 export async function registerUser(email, password, fullName) {
     try {
-
         const userCredential = await createUserWithEmailAndPassword(auth, email, password);
         const user = userCredential.user;
 
@@ -35,7 +31,11 @@ export async function registerUser(email, password, fullName) {
     }
 }
 
-
 export function loginUser(email, password) {
     return signInWithEmailAndPassword(auth, email, password);
+}
+
+// Função para enviar o e-mail de redefinição de senha
+export function resetPassword(email) {
+    return sendPasswordResetEmail(auth, email);
 }
